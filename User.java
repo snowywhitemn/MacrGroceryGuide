51797bd05ed67cd431ab7a84241b6a9b59262ab1
ww6684ni <60165855+snowywhitemn@users.noreply.github.com>
2020-10-06 20:28:20 -0500

    snowymn_1062020

---
+++ User.java
@@ -0,0 +1,58 @@
+package edu.metrostate.ics.macrogroceryguide.model;
+
+/**
+ * User is the person using the application. Certain criteria needs to be collected before
+ * other features of the app will work. This class holds that information gender, age, height,
+ * weight.
+ */
+public class User {
+
+    private Assessment assesment;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+
+    public User(String username, String password, String firstName, String lastName) {
+
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
